93e45d0c39be2a7d6f53e7895d1ab86b9525c790
AliSaleemHasan <ali1saleem1hasan@gmail.com>
2021-02-19 13:29:44 +0200

    move to a new firebase project

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,6 @@
 import firebase from "firebase";
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCJb4wpeec2nuFaSHzhy02EmhC_XA-mzfA",
-  authDomain: "aligram-1aade.firebaseapp.com",
-  projectId: "aligram-1aade",
-  storageBucket: "aligram-1aade.appspot.com",
-  messagingSenderId: "1077400133499",
-  appId: "1:1077400133499:web:7d58feeb65e44dd789a7de",
-  measurementId: "G-1CXZEZZSNW",
-};
-const firebaseApp = firebase.initializeApp(firebaseConfig);
+import config from  "./config.js"
+const firebaseApp = firebase.initializeApp(config);
 const db = firebaseApp.firestore();
 const auth = firebaseApp.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
